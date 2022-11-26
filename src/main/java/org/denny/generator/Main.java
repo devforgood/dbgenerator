fe54160bcc0b0875ea51005629a9e65640eee42d
DENNY_DESKTOP\devfo <devforgood@gmail.com>
2022-11-27 02:23:18 +0900

    mysql 연동

--- src/main/java/org/denny/generator/Main.java
+++ src/main/java/org/denny/generator/Main.java
@@ -5,20 +5,34 @@ import org.denny.generator.convert.sql.TableConvert;
 import org.denny.generator.object.java.ClassObject;
 import org.denny.generator.object.sql.TableObject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
-    private static String testDDLStr = "CREATE TABLE `t_building` (\n" +
-            "  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'Primary key ID',\n" +
-            "  `name` varchar(50) NOT NULL COMMENT 'name',\n" +
-            "  `sale_status` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'Sales status 1-on sale, 2-for sale, 3-sales',\n" +
-            "  `floor_space` bigint(11) DEFAULT '0' COMMENT 'Site: Units of square decimeter',\n" +
-            "  `card_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'certification time',\n" +
-            "  PRIMARY KEY (`id`)\n" +
-            "ENGINE=InnoDB AUTO_INCREMENT=197 DEFAULT CHARSET=utf8 COMMENT='Property Master';";
 
     public static void main(String[] args) {
 
-        TableObject tableObject = TableConvert.convertToTableObject(testDDLStr);
-        ClassObject classObject = ClassConvert.convertToClassObject(tableObject);
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://192.168.137.10?serverTimezone=UTC","testuser","test1234");
+            var st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SHOW CREATE TABLE game.t_building"); // ResultSet은 쿼리문을 보낸후 나온 결과를 가져올 때 사용한다.
+
+            while(rs.next()) {
+                var str = rs.getString("Create Table");
+                System.out.println(str);
+
+                TableObject tableObject = TableConvert.convertToTableObject(str);
+                ClassObject classObject = ClassConvert.convertToClassObject(tableObject);
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
 
     }
 }
